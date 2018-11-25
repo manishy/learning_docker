49d6c897f83f9384458ce9d6752cb32a554535ca
Manish <manishyadav12354@gmail.com>
2018-11-25 23:11:28 +0530

    Injected client into app

--- server.js
+++ server.js
@@ -3,8 +3,15 @@ const express = require("express");
 const app = express();
 const cookieParser = require('cookie-parser');
 const PORT = process.env.PORT;
+const defaultCs = 'postgres://localhost:5432/flower_catalog';
+const connectionString = process.env.DATABASE_URL||defaultCs;
 const lib = require("./lib/handlerLib.js");
+const {Client} = require('pg');
 
+
+app.initialize = function(client) {
+  app.client = client;
+};
 app.use(express.urlencoded({
   extended: false
 }));
@@ -21,6 +28,10 @@ app.post('/login', lib.handlePostLogin);
 app.post('/addComment', lib.handleComments);
 app.use(express.static('public'));
 
+
+const client = new Client(connectionString);
+client.connect();
+app.initialize(client);
 
 let server = http.createServer(app);
 server.listen(PORT);
