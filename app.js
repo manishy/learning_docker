1c7cd2e47f162b62be4404108b5908cb421f7cf8
Manish <manishyadav12354@gmail.com>
2018-11-25 23:30:45 +0530

    Extract get, post methods of server into app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const lib = require("./lib/handlerLib.js");
+const cookieParser = require('cookie-parser');
+
+
+app.initialize = function(client) {
+  app.client = client;
+};
+
+app.use(express.urlencoded({
+  extended: false
+}));
+app.use(cookieParser());
+app.use(lib.setSessionIdAndUser);
+// app.use(lib.insertResUtils);
+// app.use(lib.logSessionIdAndUser);
+// app.use(lib.logRequest);
+app.get('/login', lib.handleGetLogin);
+app.get('/guestBook.html', lib.serveGuestBook);
+app.post('/logout',lib.handlePostLogOut);
+app.post('/login', lib.handlePostLogin);
+app.post('/addComment', lib.handleComments);
+app.use(express.static('public'));
+
+
+module.exports = app;
